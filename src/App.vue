9ffad1e6bdabe230228c51bafaaa767566765a51
Гаранинова Снежана <sgaraninova@mail.ru>
2026-03-01 20:55:09 +0300

    Complete project with Vue, SCSS and Grid

--- src/App.vue
+++ src/App.vue
@@ -5,70 +5,48 @@ import AppCard from './components/AppCard.vue';
 const testimonials = [
   {
     id: 1,
-    avatar: "../public/images/image-daniel.jpg",
+    avatar: "image-daniel.jpg",
     name: "Daniel Clifford",
     status: "Verified Graduate",
-    title: "I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined.I honestly feel I got every penny’s worth.",
-    quote: ` "I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
-  transition and have heard some people who had an amazing experience here.I signed up
-  for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
-  The next 12 weeks was the best - and most grueling - time of my life.Since completing
-  the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup."`,
-    color: 'hsl(180, 62%, 55%)',
-    gridArea: 'Daniel'
+    title: "I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.",
+    quote: "I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here.I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life.Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup.",
+    gridArea: 'daniel',
   },
   {
     id: 2,
-    avatar: "../public/images/image-jonathan.jpg",
+    avatar: "image-jonathan.jpg",
     name: "Jonathan Walters",
     status: "Verified Graduate",
     title: "The team was very supportive and kept me motivated",
-    quote: ` “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
-  for a big company. This was one of the best investments I’ve made in myself. ”`,
-    color: 'hsl(180, 62%, 55%)',
-    gridArea: 'Jonathan'
+    quote: "I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself.",
+    gridArea: 'jonathan'
   },
   {
     id: 3,
-    avatar: "../public/images/image-jeanette.jpg",
+    avatar: "image-jeanette.jpg",
     name: "Jeanette Harmon",
     status: "Verified Graduate",
     title: "An overall wonderful and rewarding experience",
-    quote: `“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living
-  while doing something I love. ”`,
-    color: 'hsl(180, 62%, 55%)',
-    gridArea: 'Jeanette'
+    quote: "Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love.",
+    gridArea: 'jeanette'
   },
   {
     id: 4,
-    avatar: "../public/images/image-patrick.jpg",
+    avatar: "image-patrick.jpg",
     name: "Patrick Abrams",
     status: "Verified Graduate",
-    title: `"Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and
-  learning from their experiences was easy."`,
-    quote: ` “ The staff seem genuinely concerned about my progress which I find really refreshing. The program
-  gave me the confidence necessary to be able to go out in the world and present myself as a capable
-  junior developer. The standard is above the rest. You will get the personal attention you need from
-  an incredible community of smart and amazing people. ”`,
-    color: 'hsl(180, 62%, 55%)',
-    gridArea: 'Patrick '
+    title: "Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.",
+    quote: "The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people.",
+    gridArea: 'patrick'
   },
   {
     id: 5,
-    avatar: "../public/images/image-kira.jpg",
+    avatar: "image-kira.jpg",
     name: "Kira Whittle",
-    status: "  Verified Graduate",
-    title: `" Such a life-changing experience. Highly recommended!`,
-    quote: `“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from
-  professionals who can help me learn programming step by step. I was encouraged to enroll by a former
-  student of theirs who can only say wonderful things about the program. The entire curriculum and staff
-  did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team
-  project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial
-  could ever have. In fact, I’ve often referred to it during interviews as an example of my developent
-  experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers.
-  100% recommend! ”`,
-    color: 'hsl(180, 62%, 55%)',
-    gridArea: 'Kira'
+    status: "Verified Graduate",
+    title: "Such a life-changing experience. Highly recommended!",
+    quote: "Before joining the bootcamp, I’ve never written a line of code. I needed some structure fro professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my development experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend!",
+    gridArea: 'kira'
   },
 ];
 
@@ -76,15 +54,13 @@ const testimonials = [
 
 <template>
   <section class="container">
-    <ul class="features-grid">
-      <li v-for="card in testimonials" :key="card.id"
-        :class="['features-grid__item', `features-grid__item--${card.gridArea}`]">
-        <AppCard :avatar="card.avatar" :name="card.name" :status="card.status" :title="card.title"
-          :backgroundColor="card.backgroundColor" />
+    <h1 class="visually-hidden">Testimonials grid section</h1>
+    <ul class="list-grid">
+      <li v-for="card in testimonials" :key="card.id" :class="['list-grid__item', `list-grid__item--${card.gridArea}`]">
+        <AppCard v-bind="card" :theme="card.gridArea" />
       </li>
     </ul>
   </section>
-
   <Footer />
 </template>
 
@@ -93,41 +69,50 @@ const testimonials = [
 
 .container {
   margin: 0 auto;
-  padding: rem(30);
-  padding-top: fluid(56, 103, 375, 768);
-  width: 100%;
-  max-width: rem(1440);
+  padding: fluid(24, 36, 375, 1470);
+  max-width: rem(1470);
 }
 
-.features-grid {
+.list-grid {
   display: grid;
-  gap: rem(32);
   padding: 0;
   list-style: none;
-  grid-template-columns: 1fr;
+  gap: fluid(28, 30, 375, 1470);
+
+  @media (rem(600) <=width < rem(1024)) {
+    grid-template-columns: repeat(2, 1fr);
+
+
+    &__item--daniel,
+    &__item--patrick {
+      grid-column: span 2;
+    }
+  }
 
   @media (width >=rem(1024)) {
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    align-items: center;
-    grid-template-areas: "supervisor builder calc"
-      "supervisor karma   calc";
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-areas: "daniel daniel jonathan kira"
+      "jeanette patrick patrick kira";
 
     &__item {
-      &--supervisor {
-        grid-area: supervisor;
+      &--daniel {
+        grid-area: daniel;
       }
 
-      &--builder {
-        grid-area: builder;
+      &--jonathan {
+        grid-area: jonathan;
       }
 
-      &--karma {
-        grid-area: karma;
+      &--jeanette {
+        grid-area: jeanette;
       }
 
-      &--calc {
-        grid-area: calc;
+      &--patrick {
+        grid-area: patrick;
+      }
+
+      &--kira {
+        grid-area: kira;
       }
     }
   }
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,23 +5,21 @@ defineProps({
   status: String,
   title: String,
   quote: String,
-  backgroundColor: String,
+  theme: String,
 });
 </script>
 
 <template>
-  <article class="card" :style="{ backgroundColor: backgroundColor }">
+  <article :class="['card', `card--${theme}`]">
     <header class="card__header">
-      <img :src="`/images/${avatar}`" alt="" class="card__img">
-      <div class="card__header-text">
-        <h2 class="card__title">{{ name }}</h2>
-        <small class="card__">
-          {{ status }}
-        </small>
+      <img :src="`/images/${avatar}`" :alt="name" width="32" height="32" class="card__img" loading="eager">
+      <div class="card__header-wrapper">
+        <p class="card__name">{{ name }}</p>
+        <small class="card__status">{{ status }}</small>
       </div>
     </header>
-    <h3 class="card__subtitle">{{ title }}</h3>
-    <blockquote>
+    <h2 class="card__title">{{ title }}</h2>
+    <blockquote class="card__quote">
       <p class="card__text">{{ quote }}</p>
     </blockquote>
   </article>
@@ -29,4 +27,112 @@ defineProps({
 
 <style lang="scss" scoped>
 @use "../assets/styles/abstracts" as *;
+
+.card {
+  display: flex;
+  flex-direction: column;
+  padding: rem(32);
+  height: 100%;
+  border-radius: rem(8);
+
+  &__header {
+    display: flex;
+    align-items: center;
+    gap: rem(14);
+    margin-bottom: rem(16);
+  }
+
+  &__img {
+    width: rem(32);
+    height: rem(32);
+    object-fit: cover;
+    border: 2px solid;
+    border-color: transparent;
+    border-radius: 50%;
+  }
+
+  &__name {
+    margin: 0;
+    font-size: rem(13);
+    font-weight: 600;
+    line-height: 1.4;
+  }
+
+  &__status {
+    display: block;
+    font-size: rem(11);
+    opacity: .5;
+  }
+
+  &__title {
+    z-index: 1;
+    margin: 0 0 rem(16);
+    font-size: rem(20);
+    font-weight: 600;
+    line-height: 1.2;
+  }
+
+  &__quote {
+    margin: 0;
+    padding: 0;
+  }
+
+  &__text {
+    margin: 0;
+    font-size: rem(13);
+    line-height: 1.4;
+    opacity: .7;
+
+    &::before {
+      content: "“ ";
+    }
+
+    &::after {
+      content: " ”";
+    }
+  }
+
+  &--daniel {
+    color: $White;
+    background: $Purple-500 ;
+  }
+
+  &--daniel &__img {
+    border-color: $Purple-300;
+  }
+
+
+  &--jonathan {
+    color: $White;
+    background-color: $Grey-500;
+  }
+
+  &--jeanette {
+    color: $Grey-400;
+    background-color: $White;
+  }
+
+  &--patrick {
+    color: $White;
+    background-color: $Dark-blue;
+  }
+
+  &--patrick &__img {
+    border-color: $Purple-500;
+  }
+
+  &--kira {
+    color: $Grey-400;
+    background-color: $White;
+  }
+
+  @media (width >=rem(1024)) {
+    padding: rem(30);
+    box-shadow: 40px 60px 50px -10px rgb(0, 0, 0, 10%);
+
+    &--daniel {
+      background: $Purple-500 url("/images/bg-pattern-quotation.svg") no-repeat top right rem(64);
+    }
+  }
+}
 </style>
